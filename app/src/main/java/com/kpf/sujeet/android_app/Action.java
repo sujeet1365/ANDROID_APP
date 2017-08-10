d029f1640d5bc21d4ed22605b61cf81487bfc663
sujeetkumar1365 <funnychat.420@gmail.com>
2017-08-11 01:43:08 +0530

    upadte commit

--- app/src/main/java/com/kpf/sujeet/android_app/Action.java
+++ app/src/main/java/com/kpf/sujeet/android_app/Action.java
@@ -1,17 +1,35 @@
 package com.kpf.sujeet.android_app;
 
 
+import android.graphics.Bitmap;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class Action extends Fragment {
+
+    Button btn_click;
+    private Camera mCamera;
+    private CameraSurfaceView mSurfaceView;
+    public static final int MEDIA_TYPE_IMAGE = 1;
 
 
     public Action() {
@@ -22,8 +40,97 @@ public class Action extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_action, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_action, container, false);
+
+
+        FrameLayout preview = (FrameLayout) view.findViewById(R.id.camera_preview);
+        //preview.addView(mSurfaceView);
+//        clicked_image = (ImageView) findViewById(R.id.clicked_image);
+
+        mCamera = checkDeviceCamera();
+        mSurfaceView = new CameraSurfaceView(getContext(), mCamera);
+        preview.addView(mSurfaceView);
+
+        btn_click = (Button) view.findViewById(R.id.btn_click);
+        btn_click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCamera.takePicture(null, null, mPicture);
+
+            }
+        });
+
+
+        return view;
     }
+
+    private Camera checkDeviceCamera() {
+        Camera mCamera = null;
+        try {
+            mCamera = Camera.open();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return mCamera;
+    }
+
+    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+            if (pictureFile == null) {
+//                Log.d("MediaFileError", "Error creating media file, check storage permissions: "+ e.getMessage());
+                return;
+            }
+
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+            } catch (FileNotFoundException e) {
+                Log.d("File", "File not found: " + e.getMessage());
+            } catch (IOException e) {
+                Log.d("AcceseError", "Error accessing file: " + e.getMessage());
+            }
+        }
+    };
+
+    private Bitmap scaleDownBitmapImage(Bitmap bitmap, int newWidth, int newHeight) {
+        Bitmap resizedBitmap = Bitmap.createScaledBitmap(bitmap, newWidth, newHeight, true);
+        return resizedBitmap;
+    }
+
+    private static File getOutputMediaFile(int type) {
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+        
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_" + timeStamp + ".jpg");
+        }else {
+            return null;
+        }
+
+        return mediaFile;
+    }
+
 
 }
