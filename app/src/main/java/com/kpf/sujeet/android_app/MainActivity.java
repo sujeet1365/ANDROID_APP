dc2f05c0b7dcaf565e572a45f7123ceec5f789ac
sujeetkumar1365 <funnychat.420@gmail.com>
2017-08-10 20:33:57 +0530

    first commit

---
+++ app/src/main/java/com/kpf/sujeet/android_app/MainActivity.java
@@ -0,0 +1,25 @@
+package com.kpf.sujeet.android_app;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager)findViewById(R.id.viewpager);
+        tabLayout = (TabLayout)findViewById(R.id.tablayout);
+
+        PagerAdapter pager_adapter = new PagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pager_adapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
---
+++ app/src/main/java/com/kpf/sujeet/android_app/PagerAdapter.java
@@ -0,0 +1,38 @@
+package com.kpf.sujeet.android_app;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by SUJEET on 8/10/2017.
+ */
+
+public class PagerAdapter extends FragmentPagerAdapter {
+
+    String tabtitles[] = {"Actions","Show List"};
+
+    public PagerAdapter(FragmentManager fragmentManager){
+        super(fragmentManager);
+    }
+    @Override
+    public android.support.v4.app.Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new Action();
+            case 1:
+                return new Show_List();
+        }
+
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabtitles[position];
+    }
+}
